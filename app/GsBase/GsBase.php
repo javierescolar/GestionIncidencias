5a266ee365a428716a422df9ce6a846adc088cdd
JAvier <javier.escolar.developer@gmail.com>
2018-05-10 17:26:05 +0200

    primer commit

--- app/GsBase/GsBase.php
+++ app/GsBase/GsBase.php
@@ -11,8 +11,8 @@
 		private $ejercicio="madisa";
 		private $empresa="madisa mti";
 		private $aplicacion="gsges_madisa";
-		private $usua="__*alj";
-		private $passw="123";
+		private $usua="";
+		private $passw="";
 		private $ventana="aa-incidencias-web";
 
 		public $usuario;
